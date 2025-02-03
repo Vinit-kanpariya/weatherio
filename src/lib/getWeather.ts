e15b1959e3a23c44e9c3604d3ceae0e4f8d367f7
Vinit Kanpariya <vinit.k@solutlabs.com>
2025-02-03 16:47:12 +0530

    update

--- src/lib/getWeather.ts
+++ src/lib/getWeather.ts
@@ -1,5 +1,3 @@
-import { cache } from "react";
-
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 const BASE_URL_FORECAST = "https://api.openweathermap.org/data/2.5/forecast";
 const BASE_URL_CURRENT = "https://api.openweathermap.org/data/2.5/weather";
